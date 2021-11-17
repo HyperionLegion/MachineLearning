74469cd7c642c18da5858743b55b3ad82aecd068
HyperionLegion <joshua12696@gmail.com>
2021-11-17 09:38:51 -0500

    started policy update for gridworld

--- Unit 0/grid_check.py
+++ Unit 0/grid_check.py
@@ -4,13 +4,42 @@ h = int(sys.argv[2])
 
 grid = [[0.0 for x in range(w)] for y in range(h)]
 v = [[0.0 for x in range(w)] for y in range(h)]
+policies = {}
+#[right, left, down, up]
 
 #(y, x) and [y, x] format where y is height and x is width
 #grid y increases as it goes down and grid x increases as it goes left
 
 WIN_STATE = [(1, 3)]
 LOSE_STATE = [(2, 3)]
-BARRIERS = [(1,1)]
+BARRIERS = [(2,1)]
+
+#initialize policy
+for i in range(w):
+    for j in range(h):
+        if (j,i) not in BARRIERS:
+            policies[(j,i)] = [0.0, 0.0, 0.0, 0.0]
+            count = 0
+            if i+1 < w and (j, i+1) not in BARRIERS: #right
+                policies[(j,i)][0] = 1.0
+                count+=1
+            if i-1 >= 0 and (j, i-1) not in BARRIERS: #left
+                policies[(j,i)][1] = 1.0
+                count+=1
+            if j+1 < h and (j+1, i) not in BARRIERS: #down
+                policies[(j,i)][2] = 1.0
+                count+=1
+            if j-1 >= 0 and (j-1, i) not in BARRIERS: #up
+                policies[(j,i)][3] = 1.0
+                count+=1
+            if i+1 < w and (j, i+1) not in BARRIERS: #right
+                policies[(j,i)][0] = 1.0/count
+            if i-1 >= 0 and (j, i-1) not in BARRIERS: #left
+                policies[(j,i)][1] = 1.0/count
+            if j+1 < h and (j+1, i) not in BARRIERS: #down
+                policies[(j,i)][2] = 1.0/count
+            if j-1 >= 0 and (j-1, i) not in BARRIERS: #up
+                policies[(j,i)][3] = 1.0/count
 
 for i in WIN_STATE:
     grid[i[0]][i[1]] = 1.0
@@ -23,44 +52,70 @@ for x in range(h):
     print()
 
 def one_step():
+    oldv = [[0.0 for x in range(w)] for y in range(h)]
+    for j in range(h):
+        for k in range(w):
+            oldv[j][k] = v[j][k]
     for i in range(w):
         for j in range(h):
             if (j,i) not in BARRIERS:
-                oldv = v[j][i]
                 sum = 0
-                if i+1 < w and (j, i+1) not in BARRIERS:
-                    sum += (grid[j][i+1]+0.9*oldv)
-                if i-1 >= 0 and (j, i-1) not in BARRIERS:
-                    sum += (grid[j][i-1]+0.9*oldv)
-                if j+1 < h and (j+1, i) not in BARRIERS:
-                    sum += (grid[j+1][i]+0.9*oldv)
-                if j-1 >= 0 and (j-1, i) not in BARRIERS:
-                    sum += (grid[j-1][i]+0.9*oldv)
+                if i+1 < w and (j, i+1) not in BARRIERS: #right
+                    sum += (grid[j][i+1]+0.9*oldv[j][i+1])*policies[(j,i)][0]
+                if i-1 >= 0 and (j, i-1) not in BARRIERS: #left
+                    sum += (grid[j][i-1]+0.9*oldv[j][i-1])*policies[(j,i)][1]
+                if j+1 < h and (j+1, i) not in BARRIERS: #down
+                    sum += (grid[j+1][i]+0.9*oldv[j+1][i])*policies[(j,i)][2]
+                if j-1 >= 0 and (j-1, i) not in BARRIERS: #up
+                    sum += (grid[j-1][i]+0.9*oldv[j-1][i])*policies[(j,i)][3]
                 v[j][i] =  sum
 
-one_step()
+def update_policy():
+    for i in range(w):
+        for j in range(h):
+            if (j,i) not in BARRIERS:
+                sums = 0
+                probs = []
+                if i+1 < w and (j, i+1) not in BARRIERS: #right
+                    sums += v[j][i+1]
+                    probs.append(policies[(j,i)][0])
+                if i-1 >= 0 and (j, i-1) not in BARRIERS: #left
+                    sums += v[j][i-1]
+                    probs.append(policies[(j,i)][1])
+                if j+1 < h and (j+1, i) not in BARRIERS: #down
+                    sums += v[j+1][i]
+                    probs.append(policies[(j,i)][2])
+                if j-1 >= 0 and (j-1, i) not in BARRIERS: #up
+                    sums += v[j-1][i]
+                    probs.append(policies[(j,i)][0])
+                count = len(probs)
+                if sums!=0:
+                    min_prob = min(probs)
+                    if min_prob<0:
+                        min_prob = min_prob * -1
+                    if i+1 < w and (j, i+1) not in BARRIERS: #right
+                        policies[(j,i)][0] = (min_prob+policies[j,i][0])/(sums+min_prob*count)
+                    else:
+                        policies[(j,i)][0] = 0.0
+                    if i-1 >= 0 and (j, i-1) not in BARRIERS: #left
+                        policies[(j,i)][1] = (min_prob+policies[j,i][1])/(sums+min_prob*count)
+                    else:
+                        policies[(j,i)][1] = 0.0
+                    if j+1 < h and (j+1, i) not in BARRIERS: #down
+                        policies[(j,i)][2] = (min_prob+policies[j,i][2])/(sums+min_prob*count)
+                    else:
+                        policies[(j,i)][2] = 0.0
+                    if j-1 >= 0 and (j-1, i) not in BARRIERS: #up
+                        policies[(j,i)][3] = (min_prob+policies[j,i][3])/(sums+min_prob*count)
+                    else:
+                        policies[(j,i)][3] = 0.0   
+                    #update policy
+
+for i in range(50):
+    one_step()
+    #update_policy()
 print("updated v")
 for x in range(h):
     for y in range(w):
-        print(v[x][y], end=" ")
+        print(round(v[x][y],2), end=" ")
     print()
-
-def reset():
-    old_v = []
-    for j in range(h):
-        for k in range(w):
-            old_v[j][k] = v[j][k]
-            v[j][k] = 0.0
-    #v = sum of R' + gamma * V')
-    #v[j][k] = sum of (0.0 or 1.0 or -1.0) + 0.9*old_v[y][x] for neighbors
-
-#init policy
-p = []
-for j in range(h):
-    p.append([])
-    for k in range(w):
-        p[-1].append([0.25, 0.25, 0.25, 0.25])
-        # 0 - up
-        # 1 - down
-        # 2 - left
-        # 3 - right
